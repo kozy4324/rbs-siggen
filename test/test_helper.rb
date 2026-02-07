8effc1be3515abbedfff4e4f7306a8e219bc9520
Koji NAKAMURA <kozy4324@gmail.com>
2026-02-07 14:26:31 +0900

    Suppress unused variable warnings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+v = $VERBOSE
+$VERBOSE = false
 require "rbs/siggen"
+$VERBOSE = v
 
 require "minitest/autorun"
